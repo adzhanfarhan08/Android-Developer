ded9187346be3e447b0a20c93f3d9c7a6b8385b3
adzhanfarhan08 <adzhanfarhan@gmail.com>
2025-04-23 14:55:16 +0700

    feat: study update

--- Kotlin (Study)/1-hello-world/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Kotlin (Study)/1-hello-world/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.helloworld
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nameEditText: EditText
+    private lateinit var sayHelloButton: Button
+    private lateinit var sayHelloTextView: TextView
+
+    private fun initComponents() {
+        nameEditText = findViewById(R.id.nameEditText);
+        sayHelloButton = findViewById(R.id.sayHelloButton);
+        sayHelloTextView = findViewById(R.id.sayHelloTextView);
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.hello_world)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.helloWorld)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+//        val nameEditText: EditText = findViewById(R.id.nameEditText);
+//        val sayHelloButton: Button = findViewById(R.id.sayHelloButton);
+//        val sayHelloTextView: TextView = findViewById(R.id.sayHelloTextView);
+
+        initComponents();
+
+//        sayHelloTextView.text = "Hi"
+        sayHelloTextView.text = resources.getString(R.string.app_name)
+
+        sayHelloButton.setOnClickListener {
+
+            Log.d("HAN", "This is debug log")
+            Log.i("HAN", "This is info log")
+            Log.w("HAN", "This is warning log")
+            Log.e("HAN", "This is error log")
+
+
+            val name = nameEditText.text.toString();
+//            sayHelloTextView.text = "Hi $name!!"
+            sayHelloTextView.text = resources.getString(R.string.sayHelloTextView, name)
+
+            resources.getStringArray(R.array.names).forEach {
+                Log.i("HAN", it)
+            }
         }
     }
 }
